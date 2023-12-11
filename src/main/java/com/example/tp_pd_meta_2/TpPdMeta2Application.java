c2707b4b4f93154e4b63078efde353f85c40d360
DiogoPinto13 <pinto.diogo2002@gmail.com>
2023-12-11 19:59:57 +0000

    Initial commit

---
+++ src/main/java/com/example/tp_pd_meta_2/TpPdMeta2Application.java
@@ -0,0 +1,13 @@
+package com.example.tp_pd_meta_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpPdMeta2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpPdMeta2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tp_pd_meta_2/TpPdMeta2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tp_pd_meta_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpPdMeta2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
